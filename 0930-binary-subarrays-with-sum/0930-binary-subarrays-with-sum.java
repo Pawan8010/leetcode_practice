17244f4a816a0a9087871f1efe355c18edc35fd3
PawanRajput <rajputpawan9765@gmail.com>
2026-09-07 02:16:16 +0530

    Time: 1 ms (99.97%), Space: 50.9 MB (66.78%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.java
@@ -1,15 +1,43 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        int count=0;
-        for(int i=0;i<nums.length;i++){
-            int sum=0;
-            for(int j=i;j<nums.length;j++){
-                sum+=nums[j];
-                if(sum==goal){
-                    count++;
-                }
+        return atMost(nums, goal) - atMost(nums, goal - 1);
+    }
+     private int atMost(int[] nums, int goal) {
+        
+        if (goal < 0) {
+            return 0;
+        }
+
+        int left = 0;
+        int sum = 0;
+        int count = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+
+            sum += nums[right];
+
+            while (sum > goal) {
+                sum -= nums[left];
+                left++;
             }
+
+            count += right - left + 1;
         }
         return count;
+
     }
+        // brut force
+
+        // int count=0;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         if(sum==goal){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+    
 }
